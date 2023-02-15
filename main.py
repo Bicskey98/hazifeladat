6bdb9f1da13d364c559213dcd3f66e70cbaa5d0a
B3ND112 <112851709+Bicskey98@users.noreply.github.com>
2023-02-15 17:24:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#Első feladat
+#Az első példánál egyiket sem fogja kinyomtatni.
+#A második példánál pedig mindkettőt kinyomtatja de a második printet egy ' -jellel fogja kiírni.
+
+
+
+
+#Második feladat(1.)
+print("    /\n   /\n  /\n /\n/\n")
+
+#Második feladat(2.)
+print(" \\\n  \\\n   \\\n    \\\n     \\\n")
+
+#Második feladat(3.)-Ezt a feladatot a python idomár alapján csináltam.
+print("\\    /\n \\  /\n  \\/\n")
+
+#Harmadik feladat(1.)
+keresztnev = input('Add meg a keresztnevedet! ')
+vezeteknev = input('Add meg a vezetéknevedet! ')
+szuletes = int(input("Add meg a születési évedet!(Csak számokkal) "))
+csillagjegy = input("Írd le a csillagjegyedet! ")
+print("A te neved: " ,keresztnev , vezeteknev, ", üdvözöllek téged!")
+print("Születési éved:",szuletes,)
+print("Csillagjegyed:",csillagjegy,)
+
+
+
